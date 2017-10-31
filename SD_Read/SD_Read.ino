36cdf57fffe4dc2756d3c1f6fbd58f2e0f04d33c
Emerick Larkin <emerickl@ufl.edu>
2017-10-30 20:22:01 -0400

    Adding data logger files.

---
+++ SD_Read/SD_Read.ino
@@ -0,0 +1,81 @@
+/*
+  SD card datalogger
+
+ This example shows how to log data from three analog sensors
+ to an SD card using the SD library.
+
+ The circuit:
+ * analog sensors on analog ins 0, 1, and 2
+ * SD card attached to SPI bus as follows:
+ ** MOSI - pin 11
+ ** MISO - pin 12
+ ** CLK - pin 52
+ ** CS - pin 53 (for MKRZero SD: SDCARD_SS_PIN)
+
+ created  24 Nov 2010
+ modified 9 Apr 2012
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ */
+
+#include <SPI.h>
+#include <SD.h>
+
+const int chipSelect = 53;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    return;
+  }
+  Serial.println("card initialized.");
+
+  // make a string for assembling the data to log:
+  String dataString = "";
+  String output = "";
+
+  dataString = "Data";
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  File dataFile = SD.open("datalog.txt", FILE_READ);
+
+  // if the file is available, write to it:
+  if (dataFile) {
+    while (dataFile.available()) {
+      output = String(dataFile.read());
+      Serial.println(output);
+    }
+    dataFile.close();
+  }
+  // if the file isn't open, pop up an error:
+  else {
+    Serial.println("error opening datalog.txt");
+  }
+}
+
+void loop() {
+  
+}
+
+
+
+
+
+
+
+
+
